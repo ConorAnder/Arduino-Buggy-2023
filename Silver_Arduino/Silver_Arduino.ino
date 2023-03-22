fd8d3958028513c31fd2c6f1bf368f937d73364a
ConorAnder <125593554+ConorAnder@users.noreply.github.com>
2023-03-22 00:45:39 +0000

    Clean up
    Hopefully this will stop the issue of it disconnecting

--- Silver_Arduino/Silver_Arduino.ino
+++ Silver_Arduino/Silver_Arduino.ino
@@ -1,18 +1,17 @@
-#include <SPI.h>
 #include <WiFiNINA.h>
-#include <NewPing.h>
 
 // WiFi
 const char SSID[] = "POCO X3 Pro";
 const char PASSWORD[] = "buggy2023";
 WiFiServer server(1300);
 bool start = false;
+char ch;
 
 // Arduino Pins
 const int FORWARD = 12;
 const int BACKWARD = 21;
-const int L_SPEED = 19;
-const int R_SPEED = 6;
+const int LEN= 19;
+const int REN = 6;
 const int L_EYE = 5;
 const int R_EYE = 4;
 const int TRIG = 17;
@@ -20,34 +19,29 @@ const int ECHO = 18;
 const int R_HALL = 2;
 const int L_HALL = 3;
 
-// Interrupt Global Variables
-double Rstate;
-double Lstate;
-volatile bool Lbool;
-volatile bool Rbool;
+// Speed Global Variables
+int rSpeed;
+int lSpeed;
 
 // PID Global Variables
-double kp = 10;
-double ki = 0;
-double kd = 0;
+float kp = 10;
+float ki = 0;
+float kd = 0;
 double totalTime;
 double totalError;
 double prevTime;
 double prevError;
-const int setpoint = 10;
-double input;
+const int SETPOINT = 10;
 double out;
-double Max, Min;
 
 // Velocity Global Variables
-double LpulseCount, RpulseCount;
-int Lpulse, Rpulse;
-int LprevPulse, RprevPulse;
+volatile int LpulseCount, RpulseCount;
+int buggySpeed, objectSpeed;
 double motorTime, elapsedMTime;
 double totalObjDist, prevDist;
-NewPing sonar(TRIG, ECHO, 50);
 
 void setup() {
+  // WiFi
   Serial.begin(9600);
   WiFi.begin(SSID, PASSWORD);
   delay(5000);
@@ -55,15 +49,15 @@ void setup() {
   Serial.print(ip);
   server.begin();
 
+  // H-Bridge Data Pins and Enable Pins
   pinMode(FORWARD, OUTPUT);
   pinMode(BACKWARD, OUTPUT);
-  pinMode(L_SPEED, OUTPUT);
-  pinMode(R_SPEED, OUTPUT);
-  
+  pinMode(LEN, OUTPUT);
+  pinMode(REN, OUTPUT);
+
+  //IR Sensors
   pinMode(L_EYE, INPUT);
   pinMode(R_EYE, INPUT);
-  Rstate = HIGH;
-  Lstate = HIGH;
 
   // Velocity Measurement
   pinMode (TRIG, OUTPUT);
@@ -81,116 +75,93 @@ void setup() {
 }
 
 void loop() {
-  WiFiClient client = server.available();  
-    
+  // WiFi
+  WiFiClient client = server.available();
+
   if (client.connected()) {
-    char ch = client.read();
+    ch = client.read();
     if (ch == 'w') {
       start = true;
-      Serial.println("Go");
+      //Serial.println("Go");
     }
 
     else if (ch == 's')
       start = false;
+    //Serial.println("Stop");
   }
 
-  int dist = sonar.ping() / US_ROUNDTRIP_CM;
-  if (dist == 0)
-    dist = 45;
-  double rightMDist = RDist();
-  double leftMDist = LDist();
-  if (dist < 10 && dist > 0 || !start) {
-    Lstate = 0;
-    Rstate = 0;
+  String message = String(buggySpeed) + "," + String(objectSpeed);
+  server.println(message);
+
+  // Ultrasonic
+  int dist = distance();
+
+  if (dist < 10) {
+    start = false;
   }
 
-  else if (digitalRead(L_EYE) && digitalRead(R_EYE)) {
-    Lstate = 150;
-    Rstate = 150;
-    Lbool = false;
-  	Rbool = false;    
-  }
-
-  if (!digitalRead(L_EYE))
-    Lbool = true;
-  
-  if (!digitalRead(R_EYE))
-    Rbool = true;
-
-  if (Lbool && start && dist > 9) {
-    Lstate = 150;
-    Rstate = 90;
-  }
-
-  if (Rbool && start && dist > 9) {
-    Lstate = 90;
-    Rstate = 150;
+  else {
+    start = true;
   }
 
   if (dist > 9 && dist < 40) {
     totalObjDist += dist - prevDist;
     prevDist = dist;
+    out = PID(dist);
   }
+  
   else {
     totalObjDist = 0;
     prevDist = 0;
+    out = 150;
   }
 
-  double buggySpeed, objectSpeed;
-  if (LpulseCount > 100) {
-    elapsedMTime = millis() - motorTime;
-    buggySpeed = velocity(rightMDist, leftMDist);
-    objectSpeed = objectVel(totalObjDist, buggySpeed); 
-    totalObjDist = 0;
-    motorTime = millis();
-    RpulseCount = 0;
-    LpulseCount = 0;
+  // Encoder and Ultrasonic
+  calcVelocity();
+
+  // Infrared Sensors
+  if (!digitalRead(L_EYE)) {
+    lSpeed = 150;
+    rSpeed = 90;
   }
 
-  client.print(dist);
-
-  input = dist;
-  if (input > 9 && input < 40) {
-    out = PID(input);
-  } else
-    out = 0;
-  Max = max(Max, out);
-  Min = min(Min, out);
-
-  if (out > 0 && start) {
-    digitalWrite(BACKWARD, HIGH);
-    digitalWrite(FORWARD, LOW);
-    analogWrite(L_SPEED, (Lstate/150)*out);
-    analogWrite(R_SPEED, (Rstate/150)*out);
+  else if (!digitalRead(R_EYE)) {
+    rSpeed = 150;
+    lSpeed = 90;
   }
-  else if (out < 0 && start) {
+
+  // Update Motor State
+  if (start) {
     digitalWrite(FORWARD, HIGH);
-    digitalWrite(BACKWARD, LOW);
-    analogWrite(L_SPEED, (Lstate/150)*(-1*out));
-    analogWrite(R_SPEED, (Rstate/150)*(-1*out));
   }
+
   else {
-    digitalWrite(FORWARD, HIGH);
-    digitalWrite(BACKWARD, LOW);
-    analogWrite(L_SPEED, (Lstate));
-    analogWrite(R_SPEED, (Rstate));
+    digitalWrite(FORWARD, LOW);
   }
-  if (Lbool || Rbool)
-    delay(100);
 
-    Serial.println(buggySpeed);
+  // Update Motor Speeds
+  analogWrite(LEN, out * (lSpeed / rSpeed));
+  analogWrite(REN, out * (rSpeed / lSpeed));
+}
+
+int distance() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  return pulseIn(ECHO, HIGH, 2900) / 58.0;
 }
 
 double PID(int distance) {
   double currTime = millis();
   double time = currTime - prevTime;
 
-  double currError = setpoint - distance;
+  double currError = SETPOINT - distance;
   totalError += (currError * time);
   double rateError = (currError - prevError) / time;
 
   double output = kp * currError + ki * totalError + kd * rateError;
-  output = map(output, Min, Max, -255, 255);
 
   prevError = currError;
   prevTime = currTime;
@@ -198,22 +169,16 @@ double PID(int distance) {
   return output;
 }
 
-double RDist() {
-  return RpulseCount / (2*3.14159*3.4);
-}
-
-double LDist() {
-  return LpulseCount / (2*3.14159*3.4);
-}
-
-double velocity(double dist1, double dist2) {
-  double meanDistance = (dist1 + dist2) / 2.0;
-  return meanDistance / (elapsedMTime/1000.0);
-}
-
-double objectVel(double distance, double speed) {
-  double RelSpeed = distance / (elapsedMTime/1000);
-  return RelSpeed + speed;
+void calcVelocity() {
+  if (LpulseCount > 100) {
+    elapsedMTime = millis() - motorTime;
+    buggySpeed = ((((2*3.14159*3.4) / RpulseCount) + ((2*3.14159*3.4) / LpulseCount)) / 2.0) / (elapsedMTime/1000.0);
+    objectSpeed = (totalObjDist / (elapsedMTime/1000)) + buggySpeed;
+    totalObjDist = 0;
+    motorTime = millis();
+    RpulseCount = 0;
+    LpulseCount = 0;
+  }
 }
 
 void Rincrement() {
