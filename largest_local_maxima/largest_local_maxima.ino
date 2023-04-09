3ee648ceb03654a5e6f01ddb888d691999161b33
ConorAnder <125593554+ConorAnder@users.noreply.github.com>
2023-04-10 00:29:22 +0100

    Add files via upload

---
+++ largest_local_maxima/largest_local_maxima.ino
@@ -0,0 +1,35 @@
+#include <vector>
+
+float max1, max2;
+
+std::vector<float> angles, distances, a;
+
+void setup() {
+  Serial.begin(9600);
+  max1 = 0;
+  max2 = 0;
+}
+
+void loop() {
+  a.clear();
+
+  //Serial.println(a[3]);
+  float b[] = { 1.1, 2.1, 3.1, 2.1, 3.1, 4.1, 5.1, 4.1, 3.1, 2.1, 1.1 };
+  for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) {
+    a.push_back(b[i]);
+  }
+
+  getPeaks(a, max1, max2);
+  Serial.println(String(max1) + " " + String(max2));
+}
+
+void getPeaks(std::vector<float> distances, float& max1, float& max2) {
+  for (int i = 1; i < distances.size() - 1; i++) {
+    if (distances[i - 1] < distances[i] && distances[i + 1] < distances[i]) {
+      if (distances[i] > max1) {
+        max2 = max1;
+        max1 = distances[i];
+      }
+    }
+  }
+}
