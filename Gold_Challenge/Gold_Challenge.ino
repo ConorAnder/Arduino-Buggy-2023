4b092c34d9a175d39644f0084eb88c3f28648698
ConorAnder <125593554+ConorAnder@users.noreply.github.com>
2023-04-10 21:14:26 +0100

    Progress
    Added code to correct the buggy's heading. Will face midway between two objects now

--- Gold_Challenge/Gold_Challenge.ino
+++ Gold_Challenge/Gold_Challenge.ino
@@ -1,6 +1,6 @@
 #include <WiFiNINA.h>
 #include <Arduino_LSM6DS3.h>
-#include <MadgwickAHRS.h>
+#include <vector>
 
 const char SSID[] = "POCO X3 Pro";
 const char PASSWORD[] = "buggy2023";
@@ -20,15 +20,17 @@ const int L_HALL = 2;
 
 volatile int LpulseCount, RpulseCount;
 
-Madgwick filter;
 float prevYaw;
 float accX, accY, accZ;
 float gyrX, gyrY, gyrZ;
 float roll, pitch, yaw;
+float revolution = 80;
+std::vector<float> angles, distances;
+int max1, max2;
 
 void setup() {
   Serial.begin(9600);
-  
+
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1)
@@ -50,42 +52,101 @@ void setup() {
 }
 
 void loop() {
-  
-  if (IMU.gyroscopeAvailable()) {
-    IMU.readGyroscope(gyrX, gyrY, gyrZ);
+  while (abs(yaw) < revolution) {
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(gyrX, gyrY, gyrZ);
+    }
+
+    yaw += (gyrZ + 3.11) * 0.001;
+
+    digitalWrite(DATA1, LOW);
+    analogWrite(DATA2, 200);
+    analogWrite(DATA3, 200);
+    digitalWrite(DATA4, LOW);
+    int length = distance();
+    if (!isinf(1.0 / length)) {
+      angles.push_back(yaw);
+      distances.push_back(1.0 / length);
+    }
+    //Serial.println(abs(yaw));
   }
-
-  if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(accX, accY, accZ);
-  }
-
-  filter.updateIMU(gyrX, gyrY, gyrZ, accX, accY, accZ);
-  roll = filter.getRoll();
-  pitch = filter.getPitch();
-  //yaw = filter.getYaw();
-  yaw += (gyrZ + 3.11) * 0.001;
-
-  //if (abs(yaw) > 360)
-  //yaw = 0;
+    yaw = 0;
 
   digitalWrite(DATA1, LOW);
-  digitalWrite(DATA2, HIGH);
-  digitalWrite(DATA3, HIGH);
+  analogWrite(DATA2, 0);
+  analogWrite(DATA3, 0);
   digitalWrite(DATA4, LOW);
-  Serial.println(abs(yaw));
 
-  while (abs(yaw) > 106) {
+  getPeaks(distances, max1, max2);
+  Serial.println(String(1 / distances[max1]) + " " + String(angles[max1]));
+  Serial.println(String(1 / distances[max2]) + " " + String(angles[max2]));
+
+  delay(5000);
+  float parkAngle = (angles[max1] + angles[max2]) / 2.0;
+  Serial.println("Park Angle: " + String(parkAngle));
+  distances.clear();
+  angles.clear();
+
+  while (abs(yaw) < parkAngle) {
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(gyrX, gyrY, gyrZ);
+    }
+
+    yaw += (gyrZ + 3.11) * 0.001;
+
     digitalWrite(DATA1, LOW);
-    digitalWrite(DATA2, LOW);
-    digitalWrite(DATA3, LOW);
+    analogWrite(DATA2, 200);
+    analogWrite(DATA3, 200);
     digitalWrite(DATA4, LOW);
-    Serial.println(abs(yaw));
+    int length = distance();
+    if (!isinf(1.0 / length)) {
+      angles.push_back(yaw);
+      distances.push_back(1.0 / length);
+    }
+    //Serial.println(abs(yaw));
+  }
+  
+  digitalWrite(DATA1, LOW);
+  analogWrite(DATA2, 0);
+  analogWrite(DATA3, 0);
+  digitalWrite(DATA4, LOW);
+  while (1)
+    ;
+}
+
+int distance() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  return pulseIn(ECHO, HIGH, 2900) / 58.0;
+}
+
+void getPeaks(std::vector<float> distances, int& max1, int& max2) {
+  float max = 0;
+  bool ascending = true;
+  for (int i = 1; i < distances.size() - 1; i++) {
+    if (distances[i] > distances[i - 1])
+      ascending = true;
+
+    else if (distances[i] < distances[i - 1])
+      ascending = false;
+
+    if (ascending && distances[i + 1] < distances[i]) {
+      if (distances[i] > max) {
+        max2 = max1;
+        max1 = i;
+        max = distances[i];
+      }
+    }
   }
 }
-  void Rincrement() {
-    RpulseCount++;
-  }
 
-  void Lincrement() {
-    LpulseCount++;
-  }
+void Rincrement() {
+  RpulseCount++;
+}
+
+void Lincrement() {
+  LpulseCount++;
+}
