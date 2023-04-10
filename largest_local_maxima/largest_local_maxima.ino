1932726daea656ea0e167e1743b0294e08747272
ConorAnder <125593554+ConorAnder@users.noreply.github.com>
2023-04-10 01:05:16 +0100

    Fixed flat peak issue
    Updated code to find local maxima even if neighboring values were equal

--- largest_local_maxima/largest_local_maxima.ino
+++ largest_local_maxima/largest_local_maxima.ino
@@ -14,7 +14,7 @@ void loop() {
   a.clear();
 
   //Serial.println(a[3]);
-  float b[] = { 1.1, 2.1, 3.1, 2.1, 3.1, 4.1, 5.1, 4.1, 3.1, 2.1, 1.1 };
+  float b[] = { 1.1, 2.1, 3.1, 3.1, 3.1, 2.1, 3.1, 4.1, 5.1, 4.1, 3.1, 2.1, 1.1 };
   for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) {
     a.push_back(b[i]);
   }
@@ -25,8 +25,15 @@ void loop() {
 
 void getPeaks(std::vector<float> distances, int& max1, int& max2) {
   float max = 0;
+  bool ascending = true;
   for (int i = 1; i < distances.size() - 1; i++) {
-    if (distances[i - 1] < distances[i] && distances[i + 1] < distances[i]) {
+    if (distances[i] > distances[i-1])
+      ascending = true;
+
+    else if (distances[i] < distances[i-1])
+      ascending = false;
+
+    if (ascending && distances[i + 1] < distances[i]) {
       if (distances[i] > max) {
         max2 = max1;
         max1 = i;
